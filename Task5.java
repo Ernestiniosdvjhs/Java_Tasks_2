3779d256be06dc2b955fe7b387ff66da328e07f9
Ernestiniosdvjhs <89936337+Ernestiniosdvjhs@users.noreply.github.com>
2022-10-28 00:13:24 +0300

    Update Task5.java

--- Task5.java
+++ Task5.java
@@ -11,9 +11,9 @@ public class Task5 {
     static void getDecimalPlaces(String s) {
         int count = 0;
         for (int i = 0; i < s.length(); i++) {
-            String s1 = String.valueOf(s.charAt(i));
-            if (s1.equals("."))
-                for (int j = 1; j < s.length()-i; i++)
+            String s1 = String.valueOf(s.charAt(i));   //к s1 приравниваем каждый элемент массива
+            if (s1.equals("."))   //сравниваем sq с точкой
+                for (int j = 1; j < s.length()-i; i++)   //считаем знаки после точки
                     count+=1;
         }
         System.out.println(count);
