8fc6d97434c1e6fac54c109df33651a8fd092454
Ernestiniosdvjhs <89936337+Ernestiniosdvjhs@users.noreply.github.com>
2022-10-28 00:19:11 +0300

    Update Task7.java

--- Task7.java
+++ Task7.java
@@ -10,12 +10,12 @@ public class Task7 {
 
     static boolean isValid(String s) {
         for (int i = 0; i < s.length(); i++) {
-            char g = s.charAt(i);
-            if (Character.isLetter(g))
+            char g = s.charAt(i);   //поочередно приравниваем каждый символ строки s пременной g типа char
+            if (Character.isLetter(g))  //проверяем, является ли g буквой, если да, то возвращаем false
                 return false;
         }
-        if (s.length()!=5)
+        if (s.length()!=5)   //если кол-во букв не равно 5, то тоже возвращаем false
             return false;
-        return true;
+        return true;  //если оба пред. условия не выполнились, то возвращаем true
     }
 }
